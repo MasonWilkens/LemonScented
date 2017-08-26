6b655a94a8a0660288b0b8b26debfc32f6204900
daylonrankin <31082961+daylonrankin@users.noreply.github.com>
2017-08-25 21:41:21 -0500

    Added enums to type effects

--- Assets/script/TypeEffect.cs
+++ Assets/script/TypeEffect.cs
@@ -227,176 +227,176 @@ public class TypeEffect{
 
 	public float typeEffectiveness(TypeEnum attType,TypeEnum defType)
 	{
-		if (TypeEnum.normal.Equals(attType)) {
-			if (defType == 13) {return 0.5f;} 
-			else if (defType == 14) {return 0.0f;}
-			else if (defType == 17) {return 0.5f;}
+		if (attType == TypeEnum.normal) {
+			if (defType == TypeEnum.rock) {return 0.5f;} 
+			else if (defType == TypeEnum.ghost) {return 0.0f;}
+			else if (defType == TypeEnum.steel) {return 0.5f;}
 		}
-		else if (attType == 2) {
-			if (defType == 2) {return 0.5f;} 
-			else if (defType == 3) {return 0.5f;}
-			else if (defType == 5) {return 2.0f;}
-			else if (defType == 6) {return 2.0f;}
-			else if (defType == 12) {return 2.0f;}
-			else if (defType == 13) {return 0.5f;}
-			else if (defType == 15) {return 0.5f;}
-			else if (defType == 17) {return 2.0f;}
+		else if (attType == TypeEnum.fire) {
+			if (defType == TypeEnum.fire) {return 0.5f;} 
+			else if (defType == TypeEnum.water) {return 0.5f;}
+			else if (defType == TypeEnum.grass) {return 2.0f;}
+			else if (defType == TypeEnum.ice) {return 2.0f;}
+			else if (defType == TypeEnum.bug) {return 2.0f;}
+			else if (defType == TypeEnum.rock) {return 0.5f;}
+			else if (defType == TypeEnum.dragon) {return 0.5f;}
+			else if (defType == TypeEnum.steel) {return 2.0f;}
 
 		}
-		else if (attType == 3) {
-			if (defType == 2) {return 2.0f;} 
-			else if (defType == 3) {return 0.5f;}
-			else if (defType == 5) {return 0.5f;}
-			else if (defType == 9) {return 2.0f;}
-			else if (defType == 13) {return 2.0f;}
-			else if (defType == 15) {return 0.5f;}
+		else if (attType == TypeEnum.water) {
+			if (defType == TypeEnum.fire) {return 2.0f;} 
+			else if (defType == TypeEnum.water) {return 0.5f;}
+			else if (defType == TypeEnum.grass) {return 0.5f;}
+			else if (defType == TypeEnum.ground) {return 2.0f;}
+			else if (defType == TypeEnum.rock) {return 2.0f;}
+			else if (defType == TypeEnum.dragon) {return 0.5f;}
 
 		}
-		else if (attType == 4) {
-			if (defType == 3) {return 2.0f;} 
-			else if (defType == 4) {return 0.5f;}
-			else if (defType == 5) {return 0.5f;}
-			else if (defType == 9) {return 0.0f;}
-			else if (defType == 10) {return 2.0f;}
-			else if (defType == 15) {return 0.5f;}
+		else if (attType == TypeEnum.electric) {
+			if (defType == TypeEnum.water) {return 2.0f;} 
+			else if (defType == TypeEnum.electric) {return 0.5f;}
+			else if (defType == TypeEnum.grass) {return 0.5f;}
+			else if (defType == TypeEnum.ground) {return 0.0f;}
+			else if (defType == TypeEnum.flying) {return 2.0f;}
+			else if (defType == TypeEnum.dragon) {return 0.5f;}
 
 		}
-		else if (attType == 5) {
-			if (defType == 2) {return 0.5f;} 
-			else if (defType == 3) {return 2.0f;}
-			else if (defType == 5) {return 0.5f;}
-			else if (defType == 8) {return 0.5f;}
-			else if (defType == 9) {return 2.0f;}
-			else if (defType == 10) {return 0.5f;}
-			else if (defType == 12) {return 0.5f;}
-			else if (defType == 13) {return 1.0f;}
-			else if (defType == 15) {return 0.5f;}
-			else if (defType == 17) {return 0.5f;}
+		else if (attType == TypeEnum.grass) {
+			if (defType == TypeEnum.fire) {return 0.5f;} 
+			else if (defType == TypeEnum.water) {return 2.0f;}
+			else if (defType == TypeEnum.grass) {return 0.5f;}
+			else if (defType == TypeEnum.poison) {return 0.5f;}
+			else if (defType == TypeEnum.ground) {return 2.0f;}
+			else if (defType == TypeEnum.flying) {return 0.5f;}
+			else if (defType == TypeEnum.bug) {return 0.5f;}
+			else if (defType == TypeEnum.rock) {return 2.0f;}
+			else if (defType == TypeEnum.dragon) {return 0.5f;}
+			else if (defType == TypeEnum.steel) {return 0.5f;}
 
 		}
-		else if (attType == 6) {
-			if (defType == 2) {return 0.5f;} 
-			else if (defType == 3) {return 0.5f;}
-			else if (defType == 5) {return 2.0f;}
-			else if (defType == 6) {return 0.5f;}
-			else if (defType == 9) {return 2.0f;}
-			else if (defType == 10) {return 2.0f;}
-			else if (defType == 15) {return 0.5f;}
+		else if (attType == TypeEnum.ice) {
+			if (defType == TypeEnum.fire) {return 0.5f;} 
+			else if (defType == TypeEnum.water) {return 0.5f;}
+			else if (defType == TypeEnum.grass) {return 2.0f;}
+			else if (defType == TypeEnum.ice) {return 0.5f;}
+			else if (defType == TypeEnum.ground) {return 2.0f;}
+			else if (defType == TypeEnum.flying) {return 2.0f;}
+			else if (defType == TypeEnum.dragon) {return 0.5f;}
 
 		}
-		else if (attType == 7) {
-			if (defType == 1) {return 2.0f;} 
-			else if (defType == 6) {return 2.0f;}
-			else if (defType == 8) {return 0.5f;}
-			else if (defType == 10) {return 0.5f;}
-			else if (defType == 11) {return 0.5f;}
-			else if (defType == 12) {return 0.5f;}
-			else if (defType == 13) {return 2.0f;}
-			else if (defType == 14) {return 0.0f;}
-			else if (defType == 16) {return 2.0f;}
-			else if (defType == 17) {return 2.0f;}
-			else if (defType == 18) {return 0.5f;}
+		else if (attType == TypeEnum.fighting) {
+			if (defType == TypeEnum.normal) {return 2.0f;} 
+			else if (defType == TypeEnum.ice) {return 2.0f;}
+			else if (defType == TypeEnum.poison) {return 0.5f;}
+			else if (defType == TypeEnum.flying) {return 0.5f;}
+			else if (defType == TypeEnum.psychic) {return 0.5f;}
+			else if (defType == TypeEnum.bug) {return 0.5f;}
+			else if (defType == TypeEnum.rock) {return 2.0f;}
+			else if (defType == TypeEnum.ghost) {return 0.0f;}
+			else if (defType == TypeEnum.dark) {return 2.0f;}
+			else if (defType == TypeEnum.steel) {return 2.0f;}
+			else if (defType == TypeEnum.fairy) {return 0.5f;}
 
 		}
-		else if (attType == 8) {
-			if (defType == 5) {return 2.0f;} 
-			else if (defType == 8) {return 0.5f;}
-			else if (defType == 9) {return 0.5f;}
-			else if (defType == 13) {return 0.5f;}
-			else if (defType == 14) {return 0.5f;}
-			else if (defType == 17) {return 0.0f;}
-			else if (defType == 14) {return 2.0f;}
+		else if (attType == TypeEnum.poison) {
+			if (defType == TypeEnum.grass) {return 2.0f;} 
+			else if (defType == TypeEnum.poison) {return 0.5f;}
+			else if (defType == TypeEnum.ground) {return 0.5f;}
+			else if (defType == TypeEnum.rock) {return 0.5f;}
+			else if (defType == TypeEnum.ghost) {return 0.5f;}
+			else if (defType == TypeEnum.steel) {return 0.0f;}
+			else if (defType == TypeEnum.fairy) {return 2.0f;}
 
 		}
-		else if (attType == 9) {
-			if (defType == 2) {return 2.0f;} 
-			else if (defType == 4) {return 2.0f;}
-			else if (defType == 5) {return 0.5f;}
-			else if (defType == 8) {return 2.0f;}
-			else if (defType == 10) {return 0.0f;}
-			else if (defType == 12) {return 0.5f;}
-			else if (defType == 13) {return 2.0f;}
-			else if (defType == 17) {return 2.0f;}
+		else if (attType == TypeEnum.ground) {
+			if (defType == TypeEnum.fire) {return 2.0f;} 
+			else if (defType == TypeEnum.electric) {return 2.0f;}
+			else if (defType == TypeEnum.grass) {return 0.5f;}
+			else if (defType == TypeEnum.poison) {return 2.0f;}
+			else if (defType == TypeEnum.flying) {return 0.0f;}
+			else if (defType == TypeEnum.bug) {return 0.5f;}
+			else if (defType == TypeEnum.rock) {return 2.0f;}
+			else if (defType == TypeEnum.steel) {return 2.0f;}
 
 		}
-		else if (attType == 10) {
-			if (defType == 4) {return 0.5f;} 
-			else if (defType == 5) {return 2.0f;}
-			else if (defType == 7) {return 2.0f;}
-			else if (defType == 12) {return 2.0f;}
-			else if (defType == 13) {return 0.5f;}
-			else if (defType == 17) {return 0.5f;}
+		else if (attType == TypeEnum.flying) {
+			if (defType == TypeEnum.electric) {return 0.5f;} 
+			else if (defType == TypeEnum.grass) {return 2.0f;}
+			else if (defType == TypeEnum.fighting) {return 2.0f;}
+			else if (defType == TypeEnum.bug) {return 2.0f;}
+			else if (defType == TypeEnum.rock) {return 0.5f;}
+			else if (defType == TypeEnum.steel) {return 0.5f;}
 
 		}
-		else if (attType == 11) {
-			if (defType == 7) {return 2.0f;} 
-			else if (defType == 8) {return 2.0f;}
-			else if (defType == 11) {return 0.5f;}
-			else if (defType == 16) {return 0.0f;}
-			else if (defType == 17) {return 0.5f;}
+		else if (attType == TypeEnum.psychic) {
+			if (defType == TypeEnum.fighting) {return 2.0f;} 
+			else if (defType == TypeEnum.poison) {return 2.0f;}
+			else if (defType == TypeEnum.psychic) {return 0.5f;}
+			else if (defType == TypeEnum.dark) {return 0.0f;}
+			else if (defType == TypeEnum.steel) {return 0.5f;}
 
 		}
-		else if (attType == 12) {
-			if (defType == 2) {return 0.5f;} 
-			else if (defType == 5) {return 2.0f;}
-			else if (defType == 7) {return 0.5f;}
-			else if (defType == 8) {return 0.5f;}
-			else if (defType == 10) {return 0.5f;}
-			else if (defType == 11) {return 2.0f;}
-			else if (defType == 14) {return 0.5f;}
-			else if (defType == 16) {return 2.0f;}
-			else if (defType == 17) {return 0.5f;}
-			else if (defType == 18) {return 0.5f;}
+		else if (attType == TypeEnum.bug) {
+			if (defType == TypeEnum.fire) {return 0.5f;} 
+			else if (defType == TypeEnum.grass) {return 2.0f;}
+			else if (defType == TypeEnum.fighting) {return 0.5f;}
+			else if (defType == TypeEnum.poison) {return 0.5f;}
+			else if (defType == TypeEnum.flying) {return 0.5f;}
+			else if (defType == TypeEnum.psychic) {return 2.0f;}
+			else if (defType == TypeEnum.ghost) {return 0.5f;}
+			else if (defType == TypeEnum.dark) {return 2.0f;}
+			else if (defType == TypeEnum.steel) {return 0.5f;}
+			else if (defType == TypeEnum.fairy) {return 0.5f;}
 
 		}
-		else if (attType == 13) {
-			if (defType == 2) {return 2.0f;} 
-			else if (defType == 6) {return 2.0f;}
-			else if (defType == 7) {return 0.5f;}
-			else if (defType == 9) {return 0.5f;}
-			else if (defType == 10) {return 2.0f;}
-			else if (defType == 12) {return 2.0f;}
-			else if (defType == 17) {return 0.5f;}
+		else if (attType == TypeEnum.rock) {
+			if (defType == TypeEnum.fire) {return 2.0f;} 
+			else if (defType == TypeEnum.ice) {return 2.0f;}
+			else if (defType == TypeEnum.fighting) {return 0.5f;}
+			else if (defType == TypeEnum.ground) {return 0.5f;}
+			else if (defType == TypeEnum.flying) {return 2.0f;}
+			else if (defType == TypeEnum.bug) {return 2.0f;}
+			else if (defType == TypeEnum.steel) {return 0.5f;}
 
 		}
-		else if (attType == 14) {
-			if (defType == 1) {return 0.0f;} 
-			else if (defType == 11) {return 2.0f;}
-			else if (defType == 14) {return 2.0f;}
-			else if (defType == 16) {return 0.5f;}
+		else if (attType == TypeEnum.ghost) {
+			if (defType == TypeEnum.normal) {return 0.0f;} 
+			else if (defType == TypeEnum.psychic) {return 2.0f;}
+			else if (defType == TypeEnum.ghost) {return 2.0f;}
+			else if (defType == TypeEnum.dark) {return 0.5f;}
 
 		}
-		else if (attType == 15) {
-			if (defType == 15) {return 2.0f;} 
-			else if (defType == 16) {return 0.5f;}
-			else if (defType == 17) {return 0.0f;}
+		else if (attType == TypeEnum.dragon) {
+			if (defType == TypeEnum.dragon) {return 2.0f;} 
+			else if (defType == TypeEnum.dark) {return 0.5f;}
+			else if (defType == TypeEnum.steel) {return 0.0f;}
 
 		}
-		else if (attType == 16) {
-			if (defType == 7) {return 0.5f;} 
-			else if (defType == 11) {return 2.0f;}
-			else if (defType == 14) {return 2.0f;}
-			else if (defType == 16) {return 0.5f;}
-			else if (defType == 18) {return 0.5f;}
+		else if (attType == TypeEnum.dark) {
+			if (defType == TypeEnum.fighting) {return 0.5f;} 
+			else if (defType == TypeEnum.psychic) {return 2.0f;}
+			else if (defType == TypeEnum.ghost) {return 2.0f;}
+			else if (defType == TypeEnum.dark) {return 0.5f;}
+			else if (defType == TypeEnum.fairy) {return 0.5f;}
 
 		}
-		else if (attType == 17) {
-			if (defType == 2) {return 0.5f;} 
-			else if (defType == 3) {return 0.5f;}
-			else if (defType == 4) {return 0.5f;}
-			else if (defType == 6) {return 2.0f;}
-			else if (defType == 13) {return 2.0f;}
-			else if (defType == 17) {return 0.5f;}
-			else if (defType == 18) {return 2.0f;}
+		else if (attType == TypeEnum.steel) {
+			if (defType == TypeEnum.fire) {return 0.5f;} 
+			else if (defType == TypeEnum.water) {return 0.5f;}
+			else if (defType == TypeEnum.electric) {return 0.5f;}
+			else if (defType == TypeEnum.ice) {return 2.0f;}
+			else if (defType == TypeEnum.rock) {return 2.0f;}
+			else if (defType == TypeEnum.steel) {return 0.5f;}
+			else if (defType == TypeEnum.fairy) {return 2.0f;}
 
 		}
-		else if (attType == 18) {
-			if (defType == 2) {return 0.5f;} 
-			else if (defType == 7) {return 2.0f;}
-			else if (defType == 8) {return 0.5f;}
-			else if (defType == 15) {return 2.0f;}
-			else if (defType == 16) {return 2.0f;}
-			else if (defType == 17) {return 0.5f;}
+		else if (attType == TypeEnum.fairy) {
+			if (defType == TypeEnum.fire) {return 0.5f;} 
+			else if (defType == TypeEnum.fighting) {return 2.0f;}
+			else if (defType == TypeEnum.poison) {return 0.5f;}
+			else if (defType == TypeEnum.dragon) {return 2.0f;}
+			else if (defType == TypeEnum.dark) {return 2.0f;}
+			else if (defType == TypeEnum.steel) {return 0.5f;}
 
 		}
 
