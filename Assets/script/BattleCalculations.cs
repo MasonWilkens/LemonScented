ea889a24ae06470d8d3e2d86df63aff901f48aa1
MasonWilkens <mason.wilkens@gmail.com>
2017-08-30 20:26:41 -0400

    changes
    hgere

---
+++ Assets/script/BattleCalculations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Battle calculations. Singleton: use .instance
+/// </summary>
+public class BattleCalculations : MonoBehaviour {
+
+	public static BattleCalculations instance = null;
+
+	void Awake ()
+	{
+		if (instance == null)
+			//if not, set it to this.
+			instance = this;
+		//If instance already exists:
+		else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+}
